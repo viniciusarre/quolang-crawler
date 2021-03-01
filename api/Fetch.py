75ca702f3de6b9256d38bfd7a3ed56ce3af68840
Vinícius Arré <viniciusarre@gmail.com>
2021-03-01 08:46:23 -0300

    Add comments to Fetch handler class

--- api/Fetch.py
+++ api/Fetch.py
@@ -8,6 +8,10 @@ logger = Logger()
 class Fetch(tornado.web.RequestHandler):
 
     def get(self):
+        """
+            GET method for Fetch handler. 
+            Fetches new quotes for a given author and then returns a boolean in case the request was successful
+        """
         try:
             author = self.get_query_argument("author", strip=False)
             if author is not None:
@@ -21,24 +25,10 @@ class Fetch(tornado.web.RequestHandler):
                     self.write({"success": False})
         except:
             self.write({"success": False})
-
-    def post(self):
-        try:
-            author = self.get_body_argument("author", strip=False)
-            if author is not None:
-                author = author.replace(" ", "_")
-                f = French()
-                logger.info('***** looking for author ******* ' + author)
-                res = f.url_set_up(author, "fr")
-
-                if res["status"]:
-                    self.write({"success": True})
-                else:
-                    self.write({"success": False})
-        except:
-            self.write({"success": False})
-
     def set_default_headers(self):
+        """
+            Set default headers for the requests
+        """
         logger.info("setting headers!!!")
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
